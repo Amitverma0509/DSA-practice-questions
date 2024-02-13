fd0bba9565d6ad2d61a6389d608904c49f74f61b
Amit Verma <148716794+Amitverma0509@users.noreply.github.com>
2024-02-14 04:49:19 +0530

    Update

--- Solutions/WAP to calculate the area of circle.java
+++ Solutions/WAP to calculate the area of circle.java
@@ -1,1 +1,11 @@
 import java.util.Scanner;
+class Solution {
+    public static void main(String[] args){
+        Scanner ob = new Scanner(System.in);
+        double r,area;
+        System.out.println("Enter radius of circle: ");
+        r = ob.nextDouble();
+        area = 3.14159 * r * r;
+        System.out.println("Area of circle is: "+area);
+    }
+}
